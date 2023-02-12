77b7fe2a0d124a0b8620ec1e9172c6a50aceba86
Stefan Sommer <tonprince@gmail.com>
2023-02-12 17:46:08 +0700

    first commit

---
+++ src/routes/+page.server.js
@@ -0,0 +1,16 @@
+import { BYPASS_TOKEN } from '$env/static/private';
+
+export const config = {
+  isr: {
+    expiration: false,
+    bypassToken: BYPASS_TOKEN,
+  }
+};
+
+export const prerender = true;
+export const ssr = true;
+
+export const load = () => {
+  let timestamp = new Date();
+  return { timestamp };
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let data;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<p>{data.timestamp}</p>
