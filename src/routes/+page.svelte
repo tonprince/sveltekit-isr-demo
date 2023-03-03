ca7fb5c5a1f9975dfb29da3fd02a71581410be65
Stefan Sommer <tonprince@gmail.com>
2023-03-03 11:18:38 +0700

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,5 @@
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<p>UUID generated from load function: {data.uuid}</p>
-<p>BYPASS TOKEN to invalidate route: {data.byPassToken}</p>
+<p>UUID generated from load function: <strong>{data.uuid}</strong></p>
+<p>BYPASS TOKEN to invalidate route: <strong>{data.byPassToken}</strong></p>
