fc4191ed9fb154131769cacf162c5b72020953cf
Stefan Sommer <tonprince@gmail.com>
2023-03-20 23:27:17 +0700

    updated

--- src/routes/Transfers/From-[from]/To-[to]/By-[by]/+page.server.js
+++ src/routes/Transfers/From-[from]/To-[to]/By-[by]/+page.server.js
@@ -1,12 +1,6 @@
 import { BYPASS_TOKEN } from '$env/static/private';
 
-export const config = {
-  isr: {
-    expiration: false,
-    bypassToken: BYPASS_TOKEN,
-    allowQuery: []
-  }
-};
+
 
 export async function load({params}) {
   return { from: params.from, to: params.to, by: params.by };
